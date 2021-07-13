57f6425ef25ccc9487ad15d6bbdeb257f4f989cc
Himangshu1086 <baishyahimangshu499@gmail.com>
2021-07-14 00:38:07 +0530

    update

--- server.js
+++ server.js
@@ -27,9 +27,13 @@ app.use(require('./__ROUTER/Services'))
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("frontend/build"));
 
-    app.get("*" , (req ,res)=>{
-        res.sendFile(path.resolve(__dirname , 'frontend' , 'build' , 'index.html'));
-    })
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'frontend/build/index.html'), function(err) {
+          if (err) {
+            res.status(500).send(err)
+          }
+        })
+      })
 }
 
 
