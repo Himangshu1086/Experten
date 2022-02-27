9940f27de0072e4793ab0f58b3f1b21f44b63dc2
Himangshu1086 <baishyahimangshu499@gmail.com>
2022-02-27 18:47:45 +0530

    UPDATE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Footer from './COMPONENTS/footer/Footer';
+import Footer from './COMPONENTS/FOOTER/Footer';
 import { BrowserRouter as  Router,Switch,Route } from 'react-router-dom';
 import Container__for_Card from './COMPONENTS/proffessions/card_for_profession/Container__for_Card';
 import Home from './COMPONENTS/home__page/Home';
@@ -8,7 +8,7 @@ import Navbar from './COMPONENTS/header/Header__for__others/Navbar';
 import Profession__details from './COMPONENTS/proffessions/profession__details/Profession__details';
 import Sign__In from './COMPONENTS/signIn/Sign__In';
 import Sign__Up from './COMPONENTS/signUp/Sign__Up';
-import About from './COMPONENTS/about/About';
+import About from './COMPONENTS/ABOUT/About';
 import Watch__List from './COMPONENTS/watch__list/Watch__List';
 import Admin from './COMPONENTS/ADMIN/Admin';
 import Professional_details_2 from './COMPONENTS/proffessions/profession__details/PROFESSIONS_DETAIL_2/Professional_details_2';
---
+++ frontend/src/COMPONENTS/FOOTER/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+        <div className="relative bottom-0 left-0 h-30 w-full z-50 bg-black ">   
+                 <h1 className="p-10 bottom-0 text-white text-center">© Experten | Designed by HJB@1086</h1>   
+        </div>
+        </div>
+    )
+}
+
+export default Footer
