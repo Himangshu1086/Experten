accf21acb867b3cf33cf328252800d26344fcc8f
Himangshu1086 <baishyahimangshu499@gmail.com>
2022-03-01 09:49:06 +0530

    update

---
+++ frontend/src/ADMIN/3__USER ACCOUNT/AddAdress.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddAdress() {
+  return (
+    <div>AddAdress</div>
+  )
+}
+
+export default AddAdress
---
+++ frontend/src/ADMIN/3__USER ACCOUNT/OrderList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OrderList() {
+  return (
+    <div>OrderList</div>
+  )
+}
+
+export default OrderList
